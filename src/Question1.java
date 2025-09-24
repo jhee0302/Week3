df76729e8dff8897688258b80a5ff53d80e95619
Jay <0301jhee@gmail.com>
2025-09-24 14:28:51 +0900

    COMMIT

--- src/Question1.java
+++ src/Question1.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Question1 {
-    // 사용자로부터 정수를 입력받고, 그 숫자가 짝수인지 홀수인지 판별하는 프로그램을 작성하세요.
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("정수를 입력하세요! :");
+        int n =  sc.nextInt();
+
+        if( n % 2 == 0 ) {
+            System.out.println("짝수! :");
+        } else{
+            System.out.println("홀수! :");
+        }
+    }
 }
